c5d78554bd36ad004287a4e964f107913c39ecfa
Amennmek <129428456+Amennmek@users.noreply.github.com>
2024-02-21 17:31:17 -0600

    Add files via upload

---
+++ main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  fact calc
+//
+//  Created by Amen Wolde on 3/30/23.
+//
+
+#include <stdio.h>
+
+int main() {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
